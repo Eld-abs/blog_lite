eeb9fd8c306fb41fed9c230e81598a5848c1f039
Eld-abs <big.play2005@gmail.com>
2025-08-09 15:30:41 +0600

    База изменен на  sqlite3(чтобы CI работал)  Please enter the commit message for your changes. Lines starting

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # Указан в .env
-DATABASES = {
-    "default": dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+# DATABASES = {
+#     "default": dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
